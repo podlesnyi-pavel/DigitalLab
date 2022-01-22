05a78412ac0a7e31afd447e337bc771db5816537
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2022-01-22 19:04:45 +0200

    fix certificate slider

--- scripts/main.js
+++ scripts/main.js
@@ -6,64 +6,69 @@ const slider = document.querySelector('.slider');
 const sliderVisibleContent = slider.querySelector('.slider__visible-content');
 const sliderLine = sliderVisibleContent.querySelector('.slider__line');
 const images = sliderLine.querySelectorAll('.slider__img');
+const leftButtons = slider.querySelectorAll('.slider__btn--left');
+const rightButtons = slider.querySelectorAll('.slider__btn--right');
 
 let index = 0;
 let width;
 
-slider.querySelector('.slider__btn--left').addEventListener('click', () => {
-  index--;
-
-  if (index < 0) {
-    index = images.length - 1;
-  }
-
-  rollSlider();
+leftButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    index--;
+  
+    if (index < 0) {
+      index = images.length - 1;
+    }
+  
+    rollSlider();
+  });
 });
 
-slider.querySelector('.slider__btn--right')
-  .addEventListener('click', () => {
-  index++;
-
-  if (index === images.length) {
-    index = 0;
-  }
-
-  rollSlider();
+rightButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    index++;
+  
+    if (index === images.length) {
+      index = 0;
+    }
+  
+    rollSlider();
+  });
 });
 
 // reviews slider
-// const sliderReviews = document.querySelector('.slider--reviews');
-// const sliderVisibleContentReviews = sliderReviews
-//   .querySelector('.slider__visible-content--reviews');
-// const sliderLineReviews = sliderVisibleContentReviews
-//   .querySelector('.slider__line--reviews');
+const sliderReviews = document.querySelector('.slider--reviews');
+const sliderVisibleContentReviews = sliderReviews
+  .querySelector('.slider__visible-content--reviews');
+const sliderLineReviews = sliderVisibleContentReviews
+  .querySelector('.slider__line--reviews');
 
-// let widthReviewsSlider;
-// let offset = 0;
+let widthReviewsSlider;
+let offset = 0;
 
-// sliderReviews
-//   .querySelector('.slider__btn--left--reviews')
-//   .addEventListener('click', () => {
-//     offset -= widthReviewsSlider;
+sliderReviews
+  .querySelector('.slider__btn--left--reviews')
+  .addEventListener('click', () => {
+    offset -= widthReviewsSlider;
 
-//     if (offset < 0) {
-//       offset = widthReviewsSlider;
-//     }
+    if (offset < 0) {
+      offset = widthReviewsSlider;
+    }
 
-//     sliderLineReviews.style.transform = `translate(-${offset}px)`;
-// });
+    sliderLineReviews.style.transform = `translate(-${offset}px)`;
+});
 
-// sliderReviews
-//   .querySelector('.slider__btn--right--reviews')
-//   .addEventListener('click', () => {
-//     offset += widthReviewsSlider;
+sliderReviews
+  .querySelector('.slider__btn--right--reviews')
+  .addEventListener('click', () => {
+    offset += widthReviewsSlider;
 
-//     if (offset === widthReviewsSlider * 2) {
-//       offset = 0;
-//     }
+    if (offset === widthReviewsSlider * 2) {
+      offset = 0;
+    }
 
-//     sliderLineReviews.style.transform = `translate(-${offset}px)`;
-// });
+    sliderLineReviews.style.transform = `translate(-${offset}px)`;
+});
 
 // common
 function init() {
@@ -75,7 +80,7 @@ function init() {
     image.style.height = 'auto';
   });
 
-  // widthReviewsSlider = sliderVisibleContentReviews.offsetWidth;
+  widthReviewsSlider = sliderVisibleContentReviews.offsetWidth;
 
   rollSlider();
 }
@@ -84,9 +89,8 @@ function rollSlider() {
   sliderLine.style.transform = `translate(-${+index * width}px)`;
 }
 
-// window.addEventListener('resize', init);
+window.addEventListener('resize', init);
+
 init();
-
-
 form();
 menuMobile();
